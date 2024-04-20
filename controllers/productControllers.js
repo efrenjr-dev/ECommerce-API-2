75bdf660f9e9d4ff688de6ab2f9ec019f3c4038b
efrenjr-dev <efrenjr.rodriguez@gmail.com>
2024-04-20 10:26:50 +0800

    create product controller

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,10 +1,25 @@
-const Product = require('../models/Product')
+const Product = require("../models/Product");
 
 testController = (req, res) => {
     console.log("TEST Product Controller");
     res.send({ message: "TEST Product Controller" });
 };
 
+createProduct = (req, res) => {
+    console.log("POST Create Product");
+    // return res.send({ message: "POST Create Product" });
+    const newProduct = new Product({
+        productName: req.body.productName,
+        description: req.body.description,
+        price: req.body.price,
+    });
+    newProduct
+        .save()
+        .then((result) => res.send(result))
+        .catch((err) => res.send(err));
+};
+
 module.exports = {
     testController,
+    createProduct,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { verify, verifyAdmin } = require("../auth");
 
 const productControllers = require("../controllers/productControllers");
 
-const { testController } = productControllers;
+const { testController, createProduct } = productControllers;
 
 router.get("/", testController);
+router.post("/", verify, verifyAdmin, createProduct);
 
 module.exports = router;
